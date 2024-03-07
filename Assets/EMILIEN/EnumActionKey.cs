caeb96cab655ab5ff01296621dfae822bad1a9bb
emil <ehp84@sfr.fr>
2024-03-07 01:29:01 +0100

    début de la scene de menu

---
+++ Assets/EMILIEN/EnumActionKey.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnumActionKey{
+  public enum ActionKey{
+        forward,
+        backward,
+        left,
+        right,
+        croutch,
+        run,
+        jump,
+
+        interact,
+        throwing,
+
+        reload,
+        specialAttachment,
+        shoot,
+        aim,
+
+        abilityOne,
+        abilityTwo,
+        abilityThree,
+
+        map,
+        tchat,
+        stat
+  }
+}
--- Assets/EMILIEN/InputManager.cs
+++ Assets/EMILIEN/InputManager.cs
@@ -4,68 +4,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class KeyManager{
+public class KeyManager {
+    public EnumActionKey enumActionKey = new EnumActionKey();
     public Dictionary<Enum, KeyCode> keyCodeDictionary = new Dictionary<Enum, KeyCode>();
 
-    // ["forwardKey" ; KeyCode.Z], ["leftKey" ; KeyCode.Q]
-
-
-    [Header("mouvement")]
-    public KeyCode forwardKey = KeyCode.Z;
-    public KeyCode backKey = KeyCode.S;
-    public KeyCode leftKey = KeyCode.Q;
-    public KeyCode rightkKey = KeyCode.D;
-    public KeyCode runKey = KeyCode.LeftShift;
-    public KeyCode croutchkKey = KeyCode.LeftControl;
-    public KeyCode jumpKey = KeyCode.Space;
-
-
-    [Header("interaction")]
-    public KeyCode interactKey = KeyCode.F;
-    public KeyCode throwKey = KeyCode.G;
-
-    [Header("Attack")]
-    public KeyCode reloadKey = KeyCode.R;
-    public KeyCode specialWeaponKey = KeyCode.V;
-    public KeyCode shooKey = KeyCode.Mouse0;
-    public KeyCode aimKey = KeyCode.Mouse1;
-
-    [Header("Capacity")]
-    public KeyCode abilityOneKey = KeyCode.E;
-    public KeyCode abilityTwoKey = KeyCode.A;
-    public KeyCode abilityThreeKey = KeyCode.X;
-
-    [Header("orientation & multiplayer")]
-    public KeyCode mapKey = KeyCode.M;
-    public KeyCode tchatKey = KeyCode.T;
-    public KeyCode statKey = KeyCode.Tab;
-
-    
-    void Start(){
-        keyCodeDictionary.Add(0,forwardKey);
-        keyCodeDictionary.Add(0,backKey);
-        keyCodeDictionary.Add(0,leftKey);
-        keyCodeDictionary.Add(0,rightkKey);
-        keyCodeDictionary.Add(0,runKey);
-        keyCodeDictionary.Add(0,croutchkKey);
-        keyCodeDictionary.Add(0,jumpKey);
-
-        keyCodeDictionary.Add(0,interactKey);
-        keyCodeDictionary.Add(0,throwKey);
-
-        keyCodeDictionary.Add(0,reloadKey);
-        keyCodeDictionary.Add(0,specialWeaponKey);
-        keyCodeDictionary.Add(0,shooKey);
-        keyCodeDictionary.Add(0,aimKey);
-
-        keyCodeDictionary.Add(0,abilityOneKey);
-        keyCodeDictionary.Add(0,abilityTwoKey);
-        keyCodeDictionary.Add(0,abilityThreeKey);
-
-        keyCodeDictionary.Add(0,mapKey);
-        keyCodeDictionary.Add(0,tchatKey);
-        keyCodeDictionary.Add(0,statKey);
+    private void Start(){
+        DefaultKey();
     }
+    private void DefaultKey() {
+        keyCodeDictionary.Add(EnumActionKey.ActionKey.forward, KeyCode.Z);
+        keyCodeDictionary.Add(EnumActionKey.ActionKey.left, KeyCode.Q);
+        keyCodeDictionary.Add(EnumActionKey.ActionKey.backward, KeyCode.S);
+        keyCodeDictionary.Add(EnumActionKey.ActionKey.right, KeyCode.D);
+        keyCodeDictionary.Add(EnumActionKey.ActionKey.croutch, KeyCode.LeftControl);
+        keyCodeDictionary.Add(EnumActionKey.ActionKey.run, KeyCode.LeftShift);
+        keyCodeDictionary.Add(EnumActionKey.ActionKey.jump, KeyCode.Space);
+
+        keyCodeDictionary.Add(EnumActionKey.ActionKey.interact, KeyCode.F);
+        keyCodeDictionary.Add(EnumActionKey.ActionKey.throwing, KeyCode.G);
+
+        keyCodeDictionary.Add(EnumActionKey.ActionKey.reload, KeyCode.R);
+        keyCodeDictionary.Add(EnumActionKey.ActionKey.specialAttachment, KeyCode.V);
+        keyCodeDictionary.Add(EnumActionKey.ActionKey.shoot, KeyCode.Mouse1);
+        keyCodeDictionary.Add(EnumActionKey.ActionKey.aim, KeyCode.Mouse2);
+
+        keyCodeDictionary.Add(EnumActionKey.ActionKey.abilityOne, KeyCode.E);
+        keyCodeDictionary.Add(EnumActionKey.ActionKey.abilityTwo, KeyCode.A);
+        keyCodeDictionary.Add(EnumActionKey.ActionKey.abilityThree, KeyCode.X);
+
+        keyCodeDictionary.Add(EnumActionKey.ActionKey.map, KeyCode.M);
+        keyCodeDictionary.Add(EnumActionKey.ActionKey.tchat, KeyCode.T);
+        keyCodeDictionary.Add(EnumActionKey.ActionKey.stat, KeyCode.Tab);
+    }
+
 }
 
 
@@ -80,25 +51,24 @@ public class InputManager : MonoBehaviour
         if (key != selectedKey) return true;
         else return false;
     }
-
-    public void ChangeKey(string nameKey){
+    public void ChangeKey(Enum actionKey){
         foreach(KeyCode key in System.Enum.GetValues(typeof(KeyCode))){
             if (Input.GetKeyDown(key)){
                 selectedKey = key;
-                UpdateKey(nameKey, selectedKey);
+                UpdateKey(actionKey, selectedKey);
                 UpdateTextKey();
                 break;
             }
-            
+            else continue;
         }
     }
 
-    private void UpdateKey(string keyName, KeyCode newKey){
+    private void UpdateKey(Enum actionKey, KeyCode newKey){
         if (KeyIsAvailable(newKey)){
 
-            keyManager.keyCodeDictionary[keyName] = newKey;
+            keyManager.keyCodeDictionary[actionKey] = newKey;
 
-            /*if (keyManager.keyCodeDictio.TryGetValue(1, out selectedKey)){
+            /*if (keyManager.keyCodeDictionary.TryGetValue(, out selectedKey)){
 
             }*/
 
